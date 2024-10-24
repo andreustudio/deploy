f0765f930fd77509e8bffed8e89e8542c8f3e20b
andreustudio <92326779+andreustudio@users.noreply.github.com>
2024-10-24 20:39:10 +0200

    Update settings.py

--- deploy/settings.py
+++ deploy/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATOC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = ['static']
 django_heroku.settings(locals())
 
